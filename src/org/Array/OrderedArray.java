c819b26a3549b383ec6dd5a9c616acc87e0f7971
naglec13 <naglec13@gmail.com>
2019-04-11 14:54:31 +0400

    Обновлен java-файл OrderedArray

--- src/org/Array/OrderedArray.java
+++ src/org/Array/OrderedArray.java
@@ -30,4 +30,34 @@ public class OrderedArray {
                         upperBound = curIn - 1;
         }
     }
+
+    public void insert(long value){
+        int j;
+        for (j = 0; j < nElems; j++){
+            if (a[j] > value)
+                break;
+        }
+        for (int k = nElems; k > j; k--)
+            a[k] = a[k-1];
+        a[j] = value;
+        nElems++;
+    }
+
+    public boolean delete(long value){
+        int j = find(value);
+        if (j == nElems)
+            return false;
+        else {
+            for (int k = j; k < nElems; k++)
+                a[k] = a[k+1];
+            nElems--;
+            return true;
+        }
+    }
+
+    public void display(){
+        for (int j = 0; j < nElems; j++)
+            System.out.println(a[j] + " ");
+        System.out.println();
+    }
 }
